0a1728b34eb893ddc74d473948bd5da3b354e2a7
weijia18 <2658402573@qq.com>
2022-04-01 20:53:46 +0800

    update

--- kiwi-linter/src/getLangData.ts
+++ kiwi-linter/src/getLangData.ts
@@ -12,43 +12,43 @@ import { I18N_GLOB } from './const';
  * 获取对应文件的语言
  */
 export function getLangData(fileName: string) {
-  if (fs.existsSync(fileName)) {
-    return getLangJson(fileName);
-  } else {
-    return {};
-  }
+    if (fs.existsSync(fileName)) {
+        return getLangJson(fileName);
+    } else {
+        return {};
+    }
 }
 export function getI18N() {
-  const _I18N_GLOB = getCurrentProjectLangPath() || I18N_GLOB;
-  const paths = globby.sync(_I18N_GLOB);
-  const langObj = paths.reduce((prev, curr) => {
-    const filename = curr
-      .split('/')
-      .pop()
-      .replace(/\.tsx?$/, '');
-    if (filename.replace(/\.tsx?/, '') === 'index') {
-      return prev;
-    }
+    const _I18N_GLOB = getCurrentProjectLangPath() || I18N_GLOB;
+    const paths = globby.sync(_I18N_GLOB);
+    const langObj = paths.reduce((prev, curr) => {
+        const filename = curr
+            .split('/')
+            .pop()
+            .replace(/\.tsx?$/, '');
+        if (filename.replace(/\.tsx?/, '') === 'index') {
+            return prev;
+        }
 
-    const fileContent = getLangData(curr);
-    let jsObj = fileContent;
+        const fileContent = getLangData(curr);
+        let jsObj = fileContent;
 
-    if (Object.keys(jsObj).length === 0) {
-      vscode.window.showWarningMessage(`\`${curr}\` 解析失败，该文件包含的文案无法自动补全`);
-    }
+        if (Object.keys(jsObj).length === 0) {
+            vscode.window.showWarningMessage(`\`${curr}\` 解析失败，该文件包含的文案无法自动补全`);
+        }
 
-    return {
-      ...prev,
-      [filename]: jsObj
-    };
-  }, {});
-  return langObj;
+        return {
+            ...prev,
+            [filename]: jsObj
+        };
+    }, {});
+    return langObj;
 }
 /**
  * 获取全部语言, 展平
  */
 export function getSuggestLangObj() {
-  const langObj = getI18N();
-  const finalLangObj = flatten(langObj) as any;
-  return finalLangObj;
+    const langObj = getI18N();
+    const finalLangObj = flatten(langObj) as any;
+    return finalLangObj;
 }
--- kiwi-linter/src/utils.ts
+++ kiwi-linter/src/utils.ts
@@ -13,50 +13,50 @@ import * as path from 'path';
  * @param prefix
  */
 export function flatten(obj, prefix?) {
-  var propName = prefix ? prefix + '.' : '',
-    ret = {};
+    var propName = prefix ? prefix + '.' : '',
+        ret = {};
 
-  for (var attr in obj) {
-    if (_.isArray(obj[attr])) {
-      var len = obj[attr].length;
-      ret[attr] = obj[attr].join(',');
-    } else if (typeof obj[attr] === 'object') {
-      _.extend(ret, flatten(obj[attr], propName + attr));
-    } else {
-      ret[propName + attr] = obj[attr];
+    for (var attr in obj) {
+        if (_.isArray(obj[attr])) {
+            var len = obj[attr].length;
+            ret[attr] = obj[attr].join(',');
+        } else if (typeof obj[attr] === 'object') {
+            _.extend(ret, flatten(obj[attr], propName + attr));
+        } else {
+            ret[propName + attr] = obj[attr];
+        }
     }
-  }
-  return ret;
+    return ret;
 }
 
 /**
  * 查找当前位置的 Code
  */
 export function findPositionInCode(text: string, code: string) {
-  const lines = code.split('\n');
-  const lineNum = lines.findIndex(line => line.includes(text));
+    const lines = code.split('\n');
+    const lineNum = lines.findIndex(line => line.includes(text));
 
-  if (lineNum === -1) {
-    return null;
-  }
+    if (lineNum === -1) {
+        return null;
+    }
 
-  let chNum = lines[lineNum].indexOf(text);
+    let chNum = lines[lineNum].indexOf(text);
 
-  if (text.startsWith(' ')) {
-    chNum += 1;
-  }
+    if (text.startsWith(' ')) {
+        chNum += 1;
+    }
 
-  return new vscode.Position(lineNum, chNum);
+    return new vscode.Position(lineNum, chNum);
 }
 
 export function findMatchKey(langObj, text) {
-  for (const key in langObj) {
-    if (langObj[key] === text) {
-      return key;
+    for (const key in langObj) {
+        if (langObj[key] === text) {
+            return key;
+        }
     }
-  }
 
-  return null;
+    return null;
 }
 
 /**
@@ -66,92 +66,92 @@ export function findMatchKey(langObj, text) {
  * @return {string[]} Array with all file names that are inside the directory.
  */
 export const getAllFiles = dir =>
-  fs.readdirSync(dir).reduce((files, file) => {
-    // 避免读取node_modules造成性能问题
-    if (file === 'node_modules') {
-      return [...files];
-    }
-    const name = path.join(dir, file);
-    const isDirectory = fs.statSync(name).isDirectory();
-    return isDirectory ? [...files, ...getAllFiles(name)] : [...files, name];
-  }, []);
+    fs.readdirSync(dir).reduce((files, file) => {
+        // 避免读取node_modules造成性能问题
+        if (file === 'node_modules') {
+            return [...files];
+        }
+        const name = path.join(dir, file);
+        const isDirectory = fs.statSync(name).isDirectory();
+        return isDirectory ? [...files, ...getAllFiles(name)] : [...files, name];
+    }, []);
 
 /**
  * 获取文件 Json
  */
 export function getLangJson(fileName) {
-  const fileContent = fs.readFileSync(fileName, { encoding: 'utf8' });
-  let obj = fileContent.match(/export\s*default\s*({[\s\S]+);?$/)[1];
-  obj = obj.replace(/\s*;\s*$/, '');
-  let jsObj = {};
-  try {
-    jsObj = eval('(' + obj + ')');
-  } catch (err) {
-    console.log(obj);
-    console.error(err);
-  }
-  return jsObj;
+    const fileContent = fs.readFileSync(fileName, { encoding: 'utf8' });
+    let obj = fileContent.match(/export\s*default\s*({[\s\S]+);?$/)[1];
+    obj = obj.replace(/\s*;\s*$/, '');
+    let jsObj = {};
+    try {
+        jsObj = eval('(' + obj + ')');
+    } catch (err) {
+        console.log(obj);
+        console.error(err);
+    }
+    return jsObj;
 }
 
 /**
  * 获取配置，支持从vscode和配置文件(优先)中取到配置项
  */
 export const getConfiguration = text => {
-  let value = vscode.workspace.getConfiguration('vscode-i18n-linter').get(text);
-  let kiwiConfigJson = getConfigFile();
-  if (!kiwiConfigJson) {
+    let value = vscode.workspace.getConfiguration('vscode-i18n-linter').get(text);
+    let kiwiConfigJson = getConfigFile();
+    if (!kiwiConfigJson) {
+        return value;
+    }
+    const config = getLangJson(kiwiConfigJson);
+    if (text in config) {
+        value = config[text];
+    }
     return value;
-  }
-  const config = getLangJson(kiwiConfigJson);
-  if (text in config) {
-    value = config[text];
-  }
-  return value;
 };
 
 /**
  * 查找kiwi-cli配置文件
  */
 export const getConfigFile = () => {
-  let kiwiConfigJson = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.kiwirc.js`;
-  // 先找js
-  if (!fs.existsSync(kiwiConfigJson)) {
-    kiwiConfigJson = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.kiwirc.ts`;
-    //再找ts
+    let kiwiConfigJson = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.kiwirc.js`;
+    // 先找js
     if (!fs.existsSync(kiwiConfigJson)) {
-      return null;
+        kiwiConfigJson = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.kiwirc.ts`;
+        //再找ts
+        if (!fs.existsSync(kiwiConfigJson)) {
+            return null;
+        }
     }
-  }
-  return kiwiConfigJson;
+    return kiwiConfigJson;
 };
 
 /**
  * 查找kiwi-linter配置文件
  */
 export const getKiwiLinterConfigFile = () => {
-  let kiwiConfigJson = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.kiwi`;
-  // 先找js
-  if (!fs.existsSync(kiwiConfigJson)) {
-    return null;
-  }
-  return kiwiConfigJson;
+    let kiwiConfigJson = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.kiwi`;
+    // 先找js
+    if (!fs.existsSync(kiwiConfigJson)) {
+        return null;
+    }
+    return kiwiConfigJson;
 };
 
 /**
  * 获得项目配置信息中的 googleApiKey
  */
 function getGoogleApiKey() {
-  const configFile = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.kiwi`;
-  let googleApiKey = '';
+    const configFile = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.kiwi`;
+    let googleApiKey = '';
 
-  try {
-    if (fs.existsSync(configFile)) {
-      googleApiKey = JSON.parse(fs.readFileSync(configFile, 'utf8')).googleApiKey;
+    try {
+        if (fs.existsSync(configFile)) {
+            googleApiKey = JSON.parse(fs.readFileSync(configFile, 'utf8')).googleApiKey;
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
-  return googleApiKey;
+    return googleApiKey;
 }
 
 /**
@@ -160,15 +160,15 @@ function getGoogleApiKey() {
  * @param times
  */
 function retry(asyncOperation, times = 1) {
-  let runTimes = 1;
-  const handleReject = e => {
-    if (runTimes++ < times) {
-      return asyncOperation().catch(handleReject);
-    } else {
-      throw e;
-    }
-  };
-  return asyncOperation().catch(handleReject);
+    let runTimes = 1;
+    const handleReject = e => {
+        if (runTimes++ < times) {
+            return asyncOperation().catch(handleReject);
+        } else {
+            throw e;
+        }
+    };
+    return asyncOperation().catch(handleReject);
 }
 
 /**
@@ -177,80 +177,80 @@ function retry(asyncOperation, times = 1) {
  * @param ms
  */
 function withTimeout(promise, ms) {
-  const timeoutPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject(`Promise timed out after ${ms} ms.`);
-    }, ms);
-  });
-  return Promise.race([promise, timeoutPromise]);
+    const timeoutPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(`Promise timed out after ${ms} ms.`);
+        }, ms);
+    });
+    return Promise.race([promise, timeoutPromise]);
 }
 
 /**
  * 翻译中文
  */
 export function translateText(text) {
-  const googleApiKey = getGoogleApiKey();
-  const { translate: googleTranslate } = require('google-translate')(googleApiKey);
+    const googleApiKey = getGoogleApiKey();
+    const { translate: googleTranslate } = require('google-translate')(googleApiKey);
 
-  function _translateText() {
-    return withTimeout(
-      new Promise((resolve, reject) => {
-        googleTranslate(text, 'zh', 'en', (err, translation) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(translation.translatedText);
-          }
-        });
-      }),
-      3000
-    );
-  }
+    function _translateText() {
+        return withTimeout(
+            new Promise((resolve, reject) => {
+                // googleTranslate(text, 'zh', 'en', (err, translation) => {
+                //     if (err) {
+                //         reject(err);
+                //     } else {
+                //         resolve(translation.translatedText);
+                //     }
+                // });
+                resolve(text)
+            }),
+            3000
+        );
+    }
 
-  return retry(_translateText, 3);
+    return retry(_translateText, 3);
 }
 
 /**
  * 获取多项目配置
  */
 export function getTargetLangPath(currentFilePath) {
-  const configFile = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.kiwi`;
-  let targetLangPath = '';
-
-  try {
-    if (fs.existsSync(configFile)) {
-      const { projects = [] } = JSON.parse(fs.readFileSync(configFile, 'utf8'));
-      console.log(projects);
-      for (const config of projects) {
-        if (currentFilePath.indexOf(`/${config.target}/`) > -1) {
-          targetLangPath = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/${config.kiwiDir}/zh_CN/`;
-          return targetLangPath;
+    const configFile = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/.kiwi`;
+    let targetLangPath = '';
+    try {
+        if (fs.existsSync(configFile)) {
+            const { projects = [] } = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+            console.log(projects);
+            for (const config of projects) {
+                if (currentFilePath.indexOf(`/${config.target}/`) > -1) {
+                    targetLangPath = `${vscode.workspace.workspaceFolders[0].uri.fsPath}/${config.kiwiDir}/zh_CN/`;
+                    return targetLangPath;
+                }
+            }
         }
-      }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 
-  return targetLangPath;
+    return targetLangPath;
 }
 
 /**
  * 获取当前文件对应的项目路径
  */
 export function getCurrentProjectLangPath() {
-  let currentProjectLangPath = '';
-  const targetLangPath = getTargetLangPath(vscode.window.activeTextEditor.document.uri.path);
-  if (targetLangPath) {
-    currentProjectLangPath = `${targetLangPath}**/*.ts`;
-  }
-  return currentProjectLangPath;
+    let currentProjectLangPath = '';
+    const targetLangPath = getTargetLangPath(vscode.window.activeTextEditor.document.uri.path);
+    if (targetLangPath) {
+        currentProjectLangPath = `${targetLangPath}**/*.ts`;
+    }
+    return currentProjectLangPath;
 }
 
 /**
  * 获取当前文件对应的语言路径
  */
 export function getLangPrefix() {
-  const langPrefix = getTargetLangPath(vscode.window.activeTextEditor.document.uri.path);
-  return langPrefix;
+    const langPrefix = getTargetLangPath(vscode.window.activeTextEditor.document.uri.path);
+    return langPrefix;
 }
